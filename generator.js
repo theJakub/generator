2ba3d10d928e49973c1d0aba3e0b9be5e70fcd81
Scott Barrett <Scooter00030@gmail.com>
2016-08-04 13:00:16 -0600

    update generator.js notes

--- generator.js
+++ generator.js
@@ -28,26 +28,44 @@ var server = "var express = require('express');" + '\n' +
 
 "var app = express();" + '\n' +
 
+'We call the express function to create a new "Express App" This "app" will handle every request that comes in to our server we are going to configure this app to handle the requests the way that we want.' + '\n' +
+
 "var PORT = process.env.PORT || 8000;" + '\n' +
+
+'process.env.PORT is an environment (system) variable which allows an external application like Heroku to set the port for our server. Or, we use the default port of 8000. Remember! Ports below 1024 are reserved for administrator use only.' + '\n' +
 
 "var bodyParser = require('body-parser');" + '\n' +
 
+'Require (but do not yet use) the body-parser middleware (installed using "npm install --save body-parser"). This will take any POST request body, parse it, and give it to us as an object called req.body, after the request has passed through that middleware.' + '\n' +
+
 "var session = require('express-session');" + '\n' +
 
+'This does the same thing as above except that it handles our session data - it adds an object called req.session, which is unique to each user and persists for that user across requests.' + '\n' +
+
 "app.use(bodyParser.urlencoded({extended: false }));" + '\n' +
+
+'bodyParser.urlencoded() returns a function(req, res, next) which is used as this layer of middleware. We pass an options object in as the first parameter to configure the way that the bodyparser middleware function works. This handles form submit data.' + '\n' +
+
 "app.use(bodyParser.json());" + '\n' +
 
+'Same, parse application/json (json AJAX requests).' + '\n' +
 
 "app.use(express.static('public'));" + '\n' +
+
+'Serve all files and folders in "public" as-is.' + '\n' +
 
 "app.use(function(req, res, next) {" + '\n' +
 "	res.status(404);" + '\n' +
 "	res.send('Address not found');" + '\n' +
 "});" + '\n' +
 
+'If no other route matches, send a 404 error (file not found).' + '\n' +
+
 "app.listen(PORT, function() {" + '\n' +
 "	console.log('server started on ' + PORT);" + '\n' +
 "});";
+
+'Start the server!'
 
 function generateMBP(){
 	shell.exec("cd ~/Desktop; mkdir newWebpageByMBP; cd newWebpageByMBP/; touch server.js; echo " + '"' + server + '"' + " >> server.js");
